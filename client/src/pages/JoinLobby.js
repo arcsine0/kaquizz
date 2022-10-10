89c23a61f4f78569709338b649f3323a76468863
arcsine0 <raphael.caballegan@gmail.com>
2022-10-10 18:03:13 +0800

    cleaned up a bit

--- client/src/pages/JoinLobby.js
+++ client/src/pages/JoinLobby.js
@@ -61,21 +61,6 @@ export default function JoinLobby(props) {
                 {players.map((element, i) => {
                     return (<PlayerCard name={element.username}/>)
                 })}
-                {/* <div className='playerCard'>
-                    <div className='playerInfo'>
-                        <h4 className="playerName">Player 1</h4>
-                        <p><span className="currentPlayer">You</span></p>
-                    </div>
-                    <img src={defaultUserImg} className='userImg'></img>
-                </div>
-
-                <div className='playerCard'>
-                    <div className='playerInfo'>
-                        <h4 className="playerName">Player 2</h4>
-                    </div>
-                    <img src={defaultUserImg} className='userImg'></img>
-                </div>            */}
-
             </div>
 
             <div className='chatLobbyBtn'>
@@ -83,21 +68,5 @@ export default function JoinLobby(props) {
                 <i class="ri-chat-3-fill large-icon"></i>
             </div>
         </div>
-        
-
-        // i used this for testing
-        // don't bother with creating the onClick function is the task is for UI
-        // i'll just add the click functions for buttons and such when the UI is done
-
-        // <form>
-        //     <div class="mb-3">
-        //         <label for="classCode" class="form-label">Enter code to join in lobby</label>
-        //         <input type="text" class="form-control" id="classCode" aria-describedby="classCode" onChange={(event) => setInputID(event.target.value)} />
-        //         <div id="classCode" class="form-text">code is a 6 character format, i.e ABCXYZ</div>
-        //         <label for="userName" class="form-label">Enter your name</label>
-        //         <input type="text" class="form-control" id="userName" aria-describedby="userName" onChange={(event) => setUserName(event.target.value)}  />
-        //     </div>
-        //     <button type="button" class="btn btn-primary" onClick={() => submitID(userName, inputID)}>Join</button>
-        // </form>
     )
 }
