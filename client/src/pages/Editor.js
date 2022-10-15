c0f66a77c895b7e6b176465f31204c9ac95f4c62
rygailkim@gmail.com <rygailkim@gmail.com>
2022-10-15 21:08:46 +0800

    final styles????

--- client/src/pages/Editor.js
+++ client/src/pages/Editor.js
@@ -109,27 +109,15 @@ export default function Editor(props) {
                         <div className="choicesEditor">
                             <div className='choiceEditorCard yellow-border'>
                                 <input type="text" id="choice" name='choice_0' value={choiceA} placeholder="Add answer 1" onChange={(e) => setChoiceA(e.target.value)}></input>
-                                <div className="correctToggleBtn correct">
-                                    <i class="ri-checkbox-circle-fill large-icon"></i>
-                                </div>
                             </div>
                             <div className='choiceEditorCard red-border'>
                                 <input type="text" id="choice" name='choice_1' value={choiceB} placeholder="Add answer 2" onChange={(e) => setChoiceB(e.target.value)}></input>
-                                <div className="correctToggleBtn wrong">
-                                    <i class="ri-close-circle-fill large-icon"></i>
-                                </div>
                             </div>
                             <div className='choiceEditorCard green-border'>
                                 <input type="text" id="choice" name='choice_2' value={choiceC} placeholder="Add answer 3" onChange={(e) => setChoiceC(e.target.value)}></input>
-                                <div className="correctToggleBtn wrong">
-                                    <i class="ri-close-circle-fill large-icon"></i>
-                                </div>
                             </div>
                             <div className='choiceEditorCard blue-border'>
                                 <input type="text" id="choice" name='choice_3' value={choiceD} placeholder="Add answer 4" onChange={(e) => setChoiceD(e.target.value)}></input>
-                                <div className="correctToggleBtn wrong">
-                                    <i class="ri-close-circle-fill large-icon"></i>
-                                </div>
                             </div>    
                         </div>
                     </div>
